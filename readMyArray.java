acb17eaffcc4b5c84a6812f31bcc7f9f7e310336
Chris Adams <chrisadams27@gmail.com>
2019-04-18 21:55:59 -0400

    Updated to ingest CSV formatted files

--- readMyArray.java
+++ readMyArray.java
@@ -1,4 +1,10 @@
 package com.company;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
 
 /**
  * Created by cadams on 4/8/19.
@@ -6,6 +12,38 @@ package com.company;
 public class readMyArray {
 
     public static void main(String[] args) {
+        String fileName= "/Users/cadams/IdeaProjects/testproject/src/com/company/read_nums.csv";
+        File file= new File(fileName);
+
+        // this gives you a 2-dimensional array of strings
+        List<List<String>> lines = new ArrayList<>();
+        Scanner inputStream;
+
+        try{
+            inputStream = new Scanner(file);
+
+            while(inputStream.hasNext()){
+                String line= inputStream.next();
+                String[] values = line.split(",");
+                // this adds the currently parsed line to the 2-dimensional string array
+                lines.add(Arrays.asList(values));
+            }
+
+            inputStream.close();
+        }catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        // the following code lets you iterate through the 2-dimensional array
+        int lineNo = 1;
+        for(List<String> line: lines) {
+            int columnNo = 1;
+            for (String value: line) {
+                System.out.println("Line " + lineNo + " Column " + columnNo + ": " + value);
+                columnNo++;
+            }
+            lineNo++;
+        }
         vel();
         thrust();
         Rocket rocket1 = new Rocket();
